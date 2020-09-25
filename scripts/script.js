f6421e2a0da8dba0e279be07547879d82139209c
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2020-09-26 00:15:34 +0300

    Sign Up form is ready!

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
 jQuery('document').ready(function() {
     jQuery('#sign-up-button').on('click', function() {
+        document.getElementById("close-button").style.display = "block";
         document.getElementById("form-relative-container").style.display = "flex";
-        document.getElementById("form-bg-id").style.display = "flex";
         document.getElementById("sign-up-form").style.display = "block";
-    })
+    });
+    jQuery('#form-close-button').on('click', function() {
+        document.getElementById("close-button").style.display = "none";
+        document.getElementById("form-relative-container").style.display = "none";
+        document.getElementById("sign-up-form").style.display = "none";
+    });
 });
