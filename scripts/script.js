4925529c5cf962a7572859799c107012f893a574
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2020-09-26 23:17:28 +0300

    Adaptated Sign Up and Log In Form, Created Log In form

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,28 @@
-jQuery('document').ready(function() {
-    jQuery('#sign-up-button').on('click', function() {
+jQuery('document').ready(function () {
+    if ($("body").width() <= 991) {
+        $("#sign-up-button").remove();
+        $("#login-button").remove();
+        $('.navbar-nav').append('<button class="btn btn-dark" id="login-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-controls="navbarSupportedContent">Увійти</button>');
+        $('.navbar-nav').append('<button class="btn btn-success" id="sign-up-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-controls="navbarSupportedContent">Зареєструватися</button>');
+    }
+    jQuery('#sign-up-button').on('click', function () {
         document.getElementById("close-button").style.display = "block";
         document.getElementById("form-relative-container").style.display = "flex";
-        document.getElementById("sign-up-form").style.display = "block";
+        document.getElementById("login-close-button").style.display = "none";
+        document.getElementById("login-form-container").style.display = "none";
     });
-    jQuery('#form-close-button').on('click', function() {
+    jQuery('#form-close-button').on('click', function () {
         document.getElementById("close-button").style.display = "none";
         document.getElementById("form-relative-container").style.display = "none";
-        document.getElementById("sign-up-form").style.display = "none";
+    });
+    jQuery('#login-button').on('click', function () {
+        document.getElementById("login-close-button").style.display = "block";
+        document.getElementById("login-form-container").style.display = "flex";
+        document.getElementById("close-button").style.display = "none";
+        document.getElementById("form-relative-container").style.display = "none";
+    });
+    jQuery('#login-close-button').on('click', function () {
+        document.getElementById("login-close-button").style.display = "none";
+        document.getElementById("login-form-container").style.display = "none";
     });
 });
