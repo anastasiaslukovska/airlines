794c66230f140ce1f64a79ded1b0c7a013bba509
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2020-10-18 00:23:22 +0300

    Forms and adaptations

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,19 @@ jQuery('document').ready(function () {
     }
     jQuery('#sign-up-button').on('click', function () {
         document.getElementById("close-button").style.display = "block";
-        document.getElementById("form-relative-container").style.display = "flex";
+        document.getElementById("sign-up-container").style.display = "flex";
         document.getElementById("login-close-button").style.display = "none";
         document.getElementById("login-form-container").style.display = "none";
     });
     jQuery('#form-close-button').on('click', function () {
         document.getElementById("close-button").style.display = "none";
-        document.getElementById("form-relative-container").style.display = "none";
+        document.getElementById("sign-up-container").style.display = "none";
     });
     jQuery('#login-button').on('click', function () {
         document.getElementById("login-close-button").style.display = "block";
         document.getElementById("login-form-container").style.display = "flex";
         document.getElementById("close-button").style.display = "none";
-        document.getElementById("form-relative-container").style.display = "none";
+        document.getElementById("sign-up-container").style.display = "none";
     });
     jQuery('#login-close-button').on('click', function () {
         document.getElementById("login-close-button").style.display = "none";
